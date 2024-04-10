3f3e4877c6aafdc711490cb004ea10d943137271
Christopher Holt <holt.christopher1@gmail.com>
2024-04-10 16:38:04 +1000

    trying to connect to render

--- escaperoom/settings.py
+++ escaperoom/settings.py
@@ -50,8 +50,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,10 +87,14 @@ import django_heroku
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'escaperooms'
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'escaperooms'
+    # }
+        'default': dj_database_url.config(        
+            # Replace this value with your local database's connection string.        
+            default='postgresql://postgres:postgres@localhost:5432/mysite',        
+            conn_max_age=600    )
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
@@ -131,6 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Add this variable to specify where successful logins should redirect to
 LOGIN_REDIRECT_URL = '/rooms/'

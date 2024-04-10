82a59a6822719815d46b9c4b57f9c93ce3bc56ac
Christopher Holt <holt.christopher1@gmail.com>
2024-04-10 17:27:20 +1000

    trying to add superuser from the build.sh

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ python3 manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python3 manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python3 manage.py createsuperuser --no-input
+fi
